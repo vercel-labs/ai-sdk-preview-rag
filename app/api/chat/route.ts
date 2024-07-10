8289c802a089b13754f31e340c2b22d6a9d57777
Nico Albanese <nico.albanese@vercel.com>
2024-07-10 18:18:40 +0100

    updates

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -13,10 +13,13 @@ export async function POST(req: Request) {
   const result = await streamText({
     model: openai("gpt-4o"),
     messages: convertToCoreMessages(messages),
-    system: `You are a helpful assistant acting as the users' second brain. Check your knowledge base before answering any questions.
+    system: `You are a helpful assistant acting as the users' second brain.
+    Be sure to getInformation from your knowledge base before answering any questions.
     ONLY respond to questions using information from tool calls.
     if no relevant information is found in the tool calls, respond, "Sorry, I don't know."
+    Be sure to adhere to any instructions in tool calls ie. if they say to responsd like "...", do exactly that.
     If the relevant information is not a direct match to the users prompt, you can be creative in deducing the answer.
+    Keep responses short and concise. Answer in a single sentence where possible.
     If you are unsure, use the getInformation tool and you can use common sense to reason based on the information you do have.`,
     tools: {
       addResource: tool({
