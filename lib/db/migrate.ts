65047b0f55115b7e989424be0ba08a842476c372
Nico Albanese <nico.albanese@vercel.com>
2024-08-29 13:49:17 +0100

    update

--- lib/db/migrate.ts
+++ lib/db/migrate.ts
@@ -1,23 +1,26 @@
-// import { env } from "@/lib/env.mjs";
+import { env } from "@/lib/env.mjs";
 
 import { drizzle } from "drizzle-orm/postgres-js";
 import { migrate } from "drizzle-orm/postgres-js/migrator";
 import postgres from "postgres";
 
+
 const runMigrate = async () => {
-  if (process.env.DATABASE_URL) {
+  if (!env.DATABASE_URL) {
     throw new Error("DATABASE_URL is not defined");
   }
 
-  const connection = postgres(process.env.DATABASE_URL ?? "", { max: 1 });
 
-  const db = drizzle(connection);
+const connection = postgres(env.DATABASE_URL, { max: 1 });
+
+const db = drizzle(connection);
+
 
   console.log("⏳ Running migrations...");
 
   const start = Date.now();
 
-  await migrate(db, { migrationsFolder: "lib/db/migrations" });
+  await migrate(db, { migrationsFolder: 'lib/db/migrations' });
 
   const end = Date.now();
 
