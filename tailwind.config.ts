0a0eabb65f9eacdd2dc4e674740792bdb71ae2e4
Nico Albanese <nico.albanese@vercel.com>
2024-08-29 17:30:41 +0100

    fix dark mode

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,6 @@ const { fontFamily } = require("tailwindcss/defaultTheme")
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: ["class"],
   content: ["app/**/*.{ts,tsx}", "components/**/*.{ts,tsx}"],
   theme: {
     container: {
