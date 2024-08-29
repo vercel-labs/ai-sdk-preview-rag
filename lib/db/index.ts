bf59df85bd741477ddf817e05b0caf5c5fc738d6
Nico Albanese <nico.albanese@vercel.com>
2024-08-29 14:04:07 +0100

    working

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -2,6 +2,6 @@ import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import { env } from "@/lib/env.mjs";
 
-const client = postgres(env.DATABASE_URL);
+const client = postgres(env.DATABASE_URL!);
 export const db = drizzle(client);
 
