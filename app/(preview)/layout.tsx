59bd6ded615b022b97b1e6a0c9fb282320ddc2b4
Nico Albanese <nico.albanese@vercel.com>
2024-08-30 10:08:22 +0100

    update metadata

--- app/(preview)/layout.tsx
+++ app/(preview)/layout.tsx
@@ -3,9 +3,10 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://ai-sdk-preview-rag.vercel.app"),
   title: "Retrieval Augmented Generation Preview",
   description:
-    "Augment language model generations with vector based retrieval using Vercel AI SDK",
+    "Augment language model generations with vector based retrieval using the Vercel AI SDK",
 };
 
 export default function RootLayout({
