3591691ae80c4859e729bf04bd3e73babd84fd06
Nico Albanese <nico.albanese@vercel.com>
2024-08-30 17:11:56 +0100

    add analytics

--- app/(preview)/layout.tsx
+++ app/(preview)/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
 
 import "./globals.css";
 
@@ -16,7 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
