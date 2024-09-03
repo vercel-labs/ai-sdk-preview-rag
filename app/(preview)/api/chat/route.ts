1ccdec7e57df025c6c1775770fce754d4f18830e
Viktor Sarström <viktorsarstrom@gmail.com>
2024-09-03 08:17:03 +0200

    Fix typos in system instructions in route.ts

--- app/(preview)/api/chat/route.ts
+++ app/(preview)/api/chat/route.ts
@@ -16,12 +16,12 @@ export async function POST(req: Request) {
     system: `You are a helpful assistant acting as the users' second brain.
     Use tools on every request.
     Be sure to getInformation from your knowledge base before answering any questions.
-    If the user presents infromation about themselves, use the addResource tool to store it.
+    If the user presents information about themselves, use the addResource tool to store it.
     If a response requires multiple tools, call one tool after another without responding to the user.
     If a response requires information from an additional tool to generate a response, call the appropriate tools in order before responding to the user.
     ONLY respond to questions using information from tool calls.
     if no relevant information is found in the tool calls, respond, "Sorry, I don't know."
-    Be sure to adhere to any instructions in tool calls ie. if they say to responsd like "...", do exactly that.
+    Be sure to adhere to any instructions in tool calls ie. if they say to respond like "...", do exactly that.
     If the relevant information is not a direct match to the users prompt, you can be creative in deducing the answer.
     Keep responses short and concise. Answer in a single sentence where possible.
     If you are unsure, use the getInformation tool and you can use common sense to reason based on the information you do have.
